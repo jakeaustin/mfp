e104d89e65383dc428ee953b5b5ec44af9555183
Jake Austin <jakeaust07@gmail.com>
2014-03-30 21:39:56 -0400

    added scrollable stats, back buttons

--- A5/script.js
+++ A5/script.js
@@ -14,11 +14,30 @@ $( document ).ready(function() {
 $( "#statButton" ).click(function() {
      $("#createRoute").hide();
      $("#statList").show();
+     $("#backTwo").hide();
 });
 
-$( "#back" ).click(function() {
+$("#statBar").click(function() {
+	$("#map").hide();
+	$("#statList").show();
+	$("#backOne").hide();
+	$("#backTwo").show();
+});
+
+function tick(){
+    $('#ticker li:first').slideUp( function () { $(this).appendTo($('#ticker')).slideDown(); });
+}
+setInterval(function(){ tick () }, 5000);
+
+
+$( "#backOne" ).click(function() {
      $("#statList").hide();
      $("#createRoute").show();
+});
+
+$("#backTwo").click(function() {
+	$("#statList").hide();
+	$("#map").show();
 });
 
 $( "#goButton" ).click(function() {
